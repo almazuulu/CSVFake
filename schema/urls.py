8c4a21f0828fc9cca0f30abc7b809637cce12248
almazuulu <almazuulu@gmail.com>
2022-05-22 04:20:37 +0600

    Edit update delete viewSchemas

--- schema/urls.py
+++ schema/urls.py
@@ -4,7 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('createschema/', create_schema, name = 'create-schema'),
+    path('listschema/', list_schema, name = 'listschema'),
     path('<pk>/', create_column, name = 'create-column'),
     path('htmx/column-form/',create_column_form, name='column-form'),
-    path('htmx/column/<pk>/',detail_column, name='detail-column')
+    path('htmx/column/<pk>/',detail_column, name='detail-column'),
+    path('htmx/schema/<pk>/delete',delete_scheme, name='delete-scheme'),
+    path('htmx/schema/<pk>/edit',edit_scheme, name='edit-scheme'),
 ]
--- schema/views.py
+++ schema/views.py
@@ -1,4 +1,5 @@
 from django.forms import inlineformset_factory
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Schema, Column
 from .forms import ColumnForm, ColumnFormSet, SchemaForm
@@ -20,12 +21,6 @@ def create_schema(request):
                 column.save()
             return redirect("create-schema")
         else:
-            # schema = schemaform.save()
-            # if form.is_valid():
-            #     schema = schemaform.save()
-            #     column = form.save(commit=False)
-            #     column.schema_name = schema
-            #     column.save()
             return render(request, "partials/column_form.html", context={
                 "form": form
             })
@@ -64,48 +59,6 @@ def create_column(request, pk):
 
     return render(request, "create_column.html", context)
 
-# def create_column(request, pk):
-#     schema = Schema.objects.get(id=pk)
-#     columns = Column.objects.filter(schema_name=schema)
-#     form = SchemaForm(request.POST or None)
-#     columnForm = ColumnForm(request.POST or None)
-#
-#     #ColumnFormSet = inlineformset_factory(Schema, Column, fields={'column_name','type_column','order'}, extra=10 )
-#     #formset = ColumnFormSet(request.POST or None)
-#     if request.method == "POST":
-#         if form.is_valid() and columnForm.is_valid():
-#             form.save(commit=False)
-#             # column.schema_name = schema
-#             # column.save()
-#             # return redirect("detail-form", pk=column.id)
-#
-#             column = columnForm.save(commit=False)
-#             column.schema_name = schema
-#             column.save()
-#             return redirect('detail-column',pk=column.id)
-#         else:
-#             return render(request, "partials/column_form.html", context={
-#                 "form": form,
-#                 "columnForm":columnForm
-#             })
-#
-#     context = {
-#         "form": form,
-#         "schema":schema,
-#         "columns": columns
-#     }
-#
-#     return render(request, "create_column.html", context)
-
-# def create_column_form(request):
-#     form = SchemaForm(request.POST or None)
-#     columnForm = ColumnForm(request.POST or None)
-#
-#     context = {
-#         "form": form,
-#         "columnForm":columnForm
-#     }
-#     return render(request, "partials/column_form.html", context)
 def create_column_form(request):
 
     form = ColumnForm(request.POST or None)
@@ -114,6 +67,13 @@ def create_column_form(request):
              }
     return render(request, "partials/column_form.html", context)
 
+def list_schema(request):
+    schemas = Schema.objects.all()
+    context = {
+        "schemas":schemas
+    }
+    return render(request, 'list_schema.html', context)
+
 
 def detail_column(request, pk):
     column = get_object_or_404(Column, id=pk)
@@ -121,3 +81,32 @@ def detail_column(request, pk):
         "column": column
     }
     return render(request, "partials/detail_column.html", context)
+
+def delete_scheme(request, pk):
+    schema = Schema.objects.get(pk = pk)
+    schema.delete()
+
+    return HttpResponse('')
+
+def edit_scheme(request, pk):
+    scheme = Schema.objects.get(pk=pk)
+    schemaform = SchemaForm(request.POST or None, instance=scheme)
+    formset = ColumnFormSet(request.POST or None, instance=scheme)
+    form = ColumnForm(request.POST or None, instance=scheme)
+    # columns = Column.objects.filter(schema_name=schemaform)
+    if request.method == "POST":
+        if formset.is_valid() and schemaform.is_valid():
+            schema = schemaform.save()
+            formset.instance = schema
+            formset.save()
+
+            if form.is_valid():
+                column = form.save()
+                return redirect("listschema")
+        return redirect('listschema')
+    context = {
+        "schemaform": schemaform,
+        "formset": formset,
+        "scheme":scheme
+    }
+    return render(request,"create_schema.html", context)
