b6e7769312276ddd3c7f7c5339011b8d0d912365
almazuulu <almazuulu@gmail.com>
2022-05-22 13:42:22 +0600

    Login page

--- CSVFake/urls.py
+++ CSVFake/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from schema.views import create_column
+from schema.views import list_schema
+from django.views.generic.base import RedirectView
+from django.contrib.auth.decorators import login_required
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('schema.urls'))
+    path('', login_required(list_schema), name = 'listschema'),
+    path('schema/', include('schema.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logout.html')),
 ]
--- schema/urls.py
+++ schema/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
 from .views import *
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('createschema/', create_schema, name = 'create-schema'),
-    path('listschema/', list_schema, name = 'listschema'),
-    path('<pk>/', create_column, name = 'create-column'),
-    path('htmx/column-form/',create_column_form, name='column-form'),
-    path('htmx/column/<pk>/',detail_column, name='detail-column'),
-    path('htmx/schema/<pk>/delete',delete_scheme, name='delete-scheme'),
-    path('htmx/schema/<pk>/edit',edit_scheme, name='edit-scheme'),
-    path('generatecsv/<pk>/',generate_csv, name="generate-csv")
+
+    path('createschema/', login_required(create_schema), name = 'create-schema'),
+    path('<pk>/', login_required(create_column), name = 'create-column'),
+    path('htmx/column-form/',login_required(create_column_form), name='column-form'),
+    path('htmx/column/<pk>/',login_required(detail_column), name='detail-column'),
+    path('htmx/schema/<pk>/delete',login_required(delete_scheme), name='delete-scheme'),
+    path('htmx/schema/<pk>/edit',login_required(edit_scheme), name='edit-scheme'),
+    path('generatecsv/<pk>/',login_required(generate_csv), name="generate-csv")
 ]
