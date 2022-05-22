2e13748c0d3f5576f6486ed6b44a07628ff4b1c9
almazuulu <almazuulu@gmail.com>
2022-05-23 01:04:53 +0600

    Function to generate CSV Created

--- schema/views.py
+++ schema/views.py
@@ -1,8 +1,14 @@
+from itertools import cycle
+from random import randint
+
 from django.forms import inlineformset_factory
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from faker import Faker
 import csv
+
+
 
 from .models import Schema, Column
 from .forms import ColumnForm, ColumnFormSet, SchemaForm
@@ -113,7 +119,9 @@ def edit_scheme(request, pk):
             formset.save()
 
             if form.is_valid():
-                column = form.save()
+                column = form.save(commit=False)
+                column.schema_name = schema
+                column.save()
             # return redirect("listschema")
             return HttpResponseRedirect(reverse('listschema'))
 
@@ -131,19 +139,85 @@ def generate_csv(request, pk):
     writer = csv.writer(response)
 
     listData = []
-    # for i in schema.column_set.all():
-    #     if i.column_name.isalpha():
-    #         listData.append([f'{schema.string_charachter}{i.column_name}{schema.string_charachter}', i.order, i.type_column])
-    #     listData.append([i.column_name, i.order,i.type_column])
+
     for i in schema.column_set.all():
         listData.append([i.column_name, i.order, i.type_column])
 
     listData.sort(key=lambda x: x[1])
     listDataCSV = [i[0] for i in listData]
     print(listDataCSV)
-    writer.writerow(listDataCSV)
 
-    writer.writerow(['233','2312', '232add'])
+    fake = Faker()
+    name_person = cycle([fake.name(), fake.name()])
+    rows = []
+
+    #actual names from schemas, not type
+    full_name_person = " "
+    job_name = " "
+    email_address = " "
+    domain_name = " "
+    tel_number = " "
+    company_name = " "
+    text_content = " "
+    integerValue = 0
+    address_value = " "
+    date_value = " "
+
+    for i in listData:
+        if i[2] == 'Full name':
+            full_name_person = i[0]
+        elif i[2] == 'Job':
+            job_name = i[0]
+        elif i[2] == 'Email':
+            email_address = i[0]
+        elif i[2] == 'Domain name':
+            domain_name = i[0]
+        elif i[2] == 'Phone number':
+            tel_number = i[0]
+        elif i[2] == 'Company name':
+            company_name = i[0]
+        elif i[2] == 'Text':
+            text_content = i[0]
+        elif i[2] == 'Integer':
+            integerValue = i[0]
+        elif i[2] == 'Address':
+            address_value = i[0]
+        elif i[2] == 'Date':
+            date_value = i[0]
+
+
+    listOfTupleDict = []
+    for i in range(5):
+        for i in listDataCSV:
+            if i == full_name_person:
+                listOfTupleDict.append((full_name_person, next(name_person)))
+            elif i == job_name:
+                listOfTupleDict.append((job_name, fake.job()))
+            elif i == email_address:
+                listOfTupleDict.append((email_address, fake.email(domain=fake.url())))
+            elif i == domain_name:
+                listOfTupleDict.append((domain_name, fake.url()))
+            elif i == tel_number:
+                listOfTupleDict.append((tel_number, fake.phone_number()))
+            elif i == company_name:
+                listOfTupleDict.append((company_name, fake.company()))
+            elif i == text_content:
+                listOfTupleDict.append((text_content, fake.sentence(nb_words=10)))
+            elif i == integerValue:
+                listOfTupleDict.append((text_content, randint(0, 10)))
+            elif i == address_value:
+                address_data = fake.country()+ " "+fake.city()+" " + fake.street_address()
+                listOfTupleDict.append((address_value, address_data ))
+            elif i == date_value:
+                listOfTupleDict.append((date_value, fake.date()))
+
+        someDict = dict(listOfTupleDict)
+        rows.append(someDict)
+
+    fake_listData = [list(rows[i].values()) for i in range(len(rows))]
+    writer.writerow(listDataCSV)
+    for data in fake_listData:
+        writer.writerow(data)
 
     return response
 
